00fc9ad638f39e937aacfe25aaf5455c065991be
Thad <thad888@hotmail.com>
2022-10-24 23:59:58 -0700

    Use forEach method to add the event listeners

--- calc.js
+++ calc.js
@@ -3,44 +3,27 @@ let number1 = 0;
 let number2 = 0;
 let current_operator;
 let sign = null;
-let prevClickOperator= false; //keeps track of whether the previous click was an operator
+let prevClickOperator = false; //keeps track of whether the previous click was an operator
 
 const screenDisplay = document.getElementById("display")
 screenDisplay.textContent = displayValue;
 
-const zero = document.getElementById("zero");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const plus = document.getElementById("add");
-const minus = document.getElementById("subtract");
-const times = document.getElementById("multiply");
-const division = document.getElementById("divide");
-const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
-
-zero.addEventListener('click', () => updateNumbers("0"));
-one.addEventListener('click', () => updateNumbers("1"));
-two.addEventListener('click', () => updateNumbers("2"));
-three.addEventListener('click', () => updateNumbers("3"));
-four.addEventListener('click', () => updateNumbers("4"));
-five.addEventListener('click', () => updateNumbers("5"));
-six.addEventListener('click', () => updateNumbers("6"));
-seven.addEventListener('click', () => updateNumbers("7"));
-eight.addEventListener('click', () => updateNumbers("8"));
-nine.addEventListener('click', () => updateNumbers("9"));
-plus.addEventListener('click', () => display("+"));
-minus.addEventListener('click', () => display("-"));
-times.addEventListener('click', () => display("*"));
-division.addEventListener('click', () => display("/"));
-equals.addEventListener('click', () => display("="));
 clear.addEventListener('click', () => clearAll());
+
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {   
+        updateNumbers(number.textContent);
+    });
+});
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        display(operator.id);
+    });
+});
 
 function updateNumbers(num){
     //We assign a value to number1
@@ -67,13 +50,6 @@ function updateNumbers(num){
         displayValue += num;
         screenDisplay.textContent = displayValue;
     }
-    //We perform an operation on number1 and number2
-    else if(!(number1==0)&&!(number2==0)&& prevClickOperator){
-        number1 = operate(Number(number1),sign,Number(number2));
-        displayValue = number1;
-        screenDisplay.textContent = displayValue;
-        number2 = 0;
-    }
     prevClickOperator = false;
 }
 
@@ -92,6 +68,8 @@ function display(operator){
         screenDisplay.textContent = displayValue;
     }
     else{
+        if(sign =="/") sign = "÷";
+        if(sign =="*") sign = "×";
         displayValue = sign;
         screenDisplay.textContent = displayValue;
     }
